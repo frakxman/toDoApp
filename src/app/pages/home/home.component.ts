54b8de18d386a62591a07974e503a3e997e9afcb
Fabian Rivera <frakxman@hotmail.com>
2023-11-11 21:20:59 -0500

    Add task model and update home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +11,49 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Create application',
+      complete: false
+    },
+    {
+      id: Date.now(),
+      title: 'Create components',
+      complete: false
+    }
+  ]);
 
+  changeHandler( event: Event ) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value.trim();
+    this.addTask(newTask);
+
+    input.value = '';
+  }
+
+  addTask( title: string ) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      complete: false
+    }
+    this.tasks.update(( tasks ) => [...tasks, newTask]);
+  }
+
+  removeTask( index: number ) {
+    this.tasks.update(( tasks ) => tasks.filter(( task, position ) => position !== index));
+  }
+
+  updateTask( index: number ) {
+    this.tasks.update(( tasks ) => tasks.map(( task, position ) => {
+      if ( position === index ) {
+        return {
+          ...task,
+          complete: !task.complete
+        }
+      }
+      return task;
+    }));
+  }
 }
